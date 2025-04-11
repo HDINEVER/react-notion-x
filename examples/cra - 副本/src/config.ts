2fc1eabdbbdf971ab029b40844482aba8b6473bd
Kroos Huang <hdinever@gmail.com>
2025-04-11 12:26:24 +0800

    cra副本

---
+++ examples/cra - 副本/src/config.ts
@@ -0,0 +1,5 @@
+// TODO: change these to your own values
+// NOTE: rootNotionSpaceId is optional; set it to undefined if you don't want to
+// use it.
+export const rootNotionPageId = 'bfd7fec286ae4e3bb40a7a3a2477b77f'
+export const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
